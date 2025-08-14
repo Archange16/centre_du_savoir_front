298c7c220738fb680812b032af6b366758cf60ed
Archange16 <asathoud16@gmail.com>
2025-08-14 21:54:06 +0100

    les auth

--- components/data/social.jsx
+++ components/data/social.jsx
@@ -5,7 +5,7 @@ const Social = () => {
     return (
         <>
             <ul>
-                <li><Link href="https://web.facebook.com/people/Centre-Professionnel-Du-Savoir/61568398310360/" target="_blank"><i className="fab fa-facebook-f"></i></Link></li>
+                <li><Link href="https://web.facebook.com/people/Centre-Professionnel-Du-Savoir/61568398310360/" ><i className="fab fa-facebook-f"></i></Link></li>
                 <li><Link href="https://www.instagram.com/centreprofessionneldusavoir" target="_blank"><i className="fab fa-instagram"></i></Link></li>
                 <li><Link href="https://www.linkedin.com/company/centre-professionnel-du-savoir" target="_blank"><i className="fab fa-linkedin-in"></i></Link></li>
             </ul>            
