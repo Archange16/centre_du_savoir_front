6e2c57d505bfdf0822d8ec3d12e427ddcb9bfae8
Archange16 <asathoud16@gmail.com>
2025-08-21 18:09:00 +0100

    test

---
+++ components/pages/admin/dashboard.jsx
@@ -0,0 +1,16 @@
+import Layout from '../../../components/Dashboard/Layout';
+import StatsCards from '../../../components/Dashboard/StatsCards';
+import Charts from '../../../components/Dashboard/Charts';
+import DataTable from '../../../components/Dashboard/DataTable';
+
+const DashboardPage = () => {
+  return (
+    <Layout>
+      <StatsCards />
+      <Charts />
+      <DataTable />
+    </Layout>
+  );
+};
+
+export default DashboardPage;
--- components/pages/ui/Navbar.jsx
+++ components/pages/ui/Navbar.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { buttonVariants } from '../ui/button';
+//import { buttonVariants } from './button'; ///ui/button
 //import { HandMetal } from 'lucide-react';
 import { authOptions } from '../../../lib/auth';
 import UserAccountnav from './UserAccountnav';
